5483d5e8b3b2a2f14a4dffaac55218770d109845
Gaspare <gasmo92@gmail.com>
2023-10-11 12:56:47 +0200

    create category model

---
+++ net-il-mio-fotoalbum/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title can't be empty")]
+        [StringLength(20, ErrorMessage = "Max 20 characters")]
+        public string Name { get; set; }
+
+        public List<Photo> Photos { get; set; }
+
+        public Category() { }
+
+        public Category(string name)
+        {
+            Name = name;
+        }
+    }
+}
