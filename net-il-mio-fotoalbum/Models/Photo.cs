378c54405303a5beff1e91cf3c6a3cbf662df64e
Gaspare <gasmo92@gmail.com>
2023-10-11 12:56:28 +0200

    create photo model

---
+++ net-il-mio-fotoalbum/Models/Photo.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title can't be empty")]
+        [StringLength(20, ErrorMessage = "Max 20 characters")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description can't be empty")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Image is required")]
+        public byte[]? Image { get; set; }
+
+        [Required(ErrorMessage = "IsVisible is required")]
+        public bool IsVisible { get; set; }
+
+        public List<Category>? Categories { get; set; }
+
+        public Photo() { }
+
+        public Photo(string title, string description, byte[] image, bool isVisible)
+        {
+            Title = title;
+            Description = description;
+            Image = image;
+            IsVisible = isVisible;
+        }
+    }
+}
