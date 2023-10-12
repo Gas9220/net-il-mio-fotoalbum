7ecaebbfbfbc26ef7552594713a5052db09d0261
Gaspare <gasmo92@gmail.com>
2023-10-12 14:56:51 +0200

    add MessageController

---
+++ net-il-mio-fotoalbum/Controllers/MessageController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net_il_mio_fotoalbum.Database;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Controllers
+{
+    public class MessageController : Controller
+    {
+
+        private PhotoContext _context;
+
+        public MessageController(PhotoContext db)
+        {
+            _context = db;
+        }
+
+        public IActionResult Index()
+        {
+
+            List<Message> messages = _context.Messages.ToList();
+
+            return View(messages);
+        }
+    }
+}
