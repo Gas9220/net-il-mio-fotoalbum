49847d21ba3b1144aa5ecc0f41ea6f4f9386ec31
Gaspare <gasmo92@gmail.com>
2023-10-12 14:53:00 +0200

    add Message model

---
+++ net-il-mio-fotoalbum/Models/Message.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Email field required")]
+        [EmailAddress(ErrorMessage = "Wrong format email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Message can't be empty")]
+        public string Content { get; set; }
+
+        public Message() { }
+        public Message(string email, string content)
+        {
+            Email = email;
+            Content = content;
+        }
+    }
+}
