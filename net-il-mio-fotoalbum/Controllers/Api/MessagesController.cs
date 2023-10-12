2f2b44e73d159fda8a7f74990225b0038d6991b2
Gaspare <gasmo92@gmail.com>
2023-10-12 15:20:08 +0200

    add Message API controller

---
+++ net-il-mio-fotoalbum/Controllers/Api/MessagesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using net_il_mio_fotoalbum.Database;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+
+        private PhotoContext _context;
+
+        public MessagesController(PhotoContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult CreateMessage([FromBody] Message message)
+        {
+            if (message != null)
+            {
+                _context.Messages.Add(message);
+                _context.SaveChanges();
+
+                return Ok("Success");
+            }
+            return BadRequest(new { Message = "Unable to send message" });
+
+        }
+    }
+}
+}
