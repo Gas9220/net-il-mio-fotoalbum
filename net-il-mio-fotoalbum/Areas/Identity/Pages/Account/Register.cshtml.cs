d0cd45cbb0e6e4b19c73fa272b74109002f2a5d2
Gaspare <gasmo92@gmail.com>
2023-10-12 10:33:58 +0200

    set default user role

--- net-il-mio-fotoalbum/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ net-il-mio-fotoalbum/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -113,11 +113,11 @@ namespace net_il_mio_fotoalbum.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
-
+                await _userManager.AddToRoleAsync(user, "Photographer");
+                
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
-
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
