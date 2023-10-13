f95631cd557a3156e41d11e366653b85eae12e96
Gaspare <gasmo92@gmail.com>
2023-10-13 09:50:03 +0200

    update Photo model

---
+++ net-il-mio-fotoalbum/Migrations/20231013073556_UpdatePhotoModelWithUserId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace net_il_mio_fotoalbum.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePhotoModelWithUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Photos",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Photos");
+        }
+    }
+}
